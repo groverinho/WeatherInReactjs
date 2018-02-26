b256ae256690b12454f7f56ecd11493e6cc5e5e0
Grover <grover.irusta@mojix.com>
2018-02-26 14:55:29 -0400

    adding mediaQueries, flexbox and more styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col} from 'react-flexbox-grid';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import LocationList from './components/LocationList';
 import './App.css';
 
@@ -13,11 +16,25 @@ class App extends Component {
   render() {
     return (
     <MuiThemeProvider>
-      <div className="App">
-        <LocationList cities={cities2}
-                      onSelectedLocation={this.handleWeatherLocationClick}>
-        </LocationList>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <AppBar title="Weather App"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList cities={cities2}
+                          onSelectedLocation={this.handleWeatherLocationClick}>
+            </LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper zDepth={4}>
+              <div className="detail"></div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     </MuiThemeProvider>
     );
   }
